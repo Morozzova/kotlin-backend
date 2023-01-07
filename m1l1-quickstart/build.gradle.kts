f7dc69c9f34a0d71ba6ab05503a029831d120f33
Piligrim <morozzova@yahoo.com>
2023-01-08 00:28:33 +0300

    First app and test

---
+++ m1l1-quickstart/build.gradle.kts
@@ -0,0 +1,14 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    testImplementation(kotlin("test-junit"))
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,4 @@
+
+rootProject.name = "ok-marketplace"
+
+include("m1l1-quickstart")
